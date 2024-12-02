f5d1c512043441bf3a955e7c816bb5bb1762ffda
blwatkins <blwatkins@users.noreply.github.com>
2024-12-01 19:59:10 -0600

    Add eslint dependencies and configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2024 brittni and the polar bear LLC.
+ *
+ * This file is a part of brittni and the polar bear's p5.js TypeScript template,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+
+import eslint from '@eslint/js';
+
+import stylistic from '@stylistic/eslint-plugin'
+
+import tsEslint from 'typescript-eslint';
+
+export default tsEslint.config(
+    eslint.configs.recommended,
+    stylistic.configs['recommended-flat'],
+    ...tsEslint.configs.recommendedTypeChecked,
+    ...tsEslint.configs.strictTypeChecked,
+    ...tsEslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            /* @stylistic/eslint-plugin */
+
+            '@stylistic/brace-style': ['error', '1tbs'],
+
+            '@stylistic/no-extra-semi': 'error',
+
+            '@stylistic/function-call-argument-newline': ['error', 'consistent'],
+
+            '@stylistic/function-call-spacing': ['error', 'never'],
+
+            '@stylistic/comma-dangle': ['error', 'never'],
+
+            '@stylistic/indent': ['error',
+                4,
+                {
+                    SwitchCase: 1,
+                    FunctionDeclaration: {
+                        parameters: 'first'
+                    },
+                    FunctionExpression: {
+                        parameters: 'first'
+                    }
+                }
+            ],
+
+            '@stylistic/indent-binary-ops': 'off',
+
+            '@stylistic/member-delimiter-style': ['error', {
+                multiline: {
+                    delimiter: 'semi',
+                    requireLast: true
+                },
+                singleline: {
+                    delimiter: 'semi',
+                    requireLast: true
+                }
+            }],
+
+            '@stylistic/operator-linebreak': ['error', 'after'],
+
+            '@stylistic/quotes': ['error',
+                'single',
+                {
+                    avoidEscape: true,
+                    allowTemplateLiterals: true
+                }
+            ],
+
+            '@stylistic/semi': ['error', 'always']
+        }
+    }
+);

fdf9ae667d58bcd9672d3668c9d704a926af55c0
brittni watkins <blwatkins@users.noreply.github.com>
2024-12-16 19:32:52 -0600

    Update webpack.config.js
    Co-authored-by: azurepolarbear <106261814+azurepolarbear@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '@batpb/genart template',
+            title: 'template sketch',
             inject: 'body',
             favicon: './assets/icon/favicon.ico'
         }),

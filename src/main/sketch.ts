ee0b579af6f9426c47bb06ff3afb82904d2413aa
blwatkins <blwatkins@users.noreply.github.com>
2025-06-24 12:29:31 -0500

    Update copyright.

--- src/main/sketch.ts
+++ src/main/sketch.ts
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (C) 2024 brittni and the polar bear LLC.
+ * Copyright (C) 2024-2025 brittni and the polar bear LLC.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
